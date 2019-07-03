8c753db619bc79efa1faa9f71ca1a00f1359f12e
alishabelle <alishabelle98@gmail.com>
2019-07-03 20:04:46 +0000

    push filestorage file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import json
+
+
+class FileStorage:
+    """ class used to store and retrieve stuff from json """
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = obj.__class__.__name__ + '.' + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        for key, val in FileStorage.__objects.items():
+            if not isinstance(val, dict):
+                FileStorage.__objects[key] = val.to_dict()
+        with open(FileStorage.__file_path, "w") as write_file:
+            json.dump(FileStorage.__objects, write_file)
+
+    def reload(self):
+        import os.path
+        from models.base_model import BaseModel
+
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as my_file:
+                FileStorage.__objects = json.load(my_file)
+        for key, val in FileStorage.__objects.items():
+            new_obj = BaseModel(None, **val)
+            FileStorage.__objects[key] = new_obj
