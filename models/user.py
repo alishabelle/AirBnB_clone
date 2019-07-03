558b6ffc12149253a922746aa8fd7074112e13b4
alishabelle <alishabelle98@gmail.com>
2019-07-03 22:30:49 +0000

    push updated file

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ write a user class that inherits from basemodel """
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
@@ -8,3 +9,10 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **wouldbekwargs):
+        """ ititializing these user class """
+        if (wouldbekwargs.get('id') is not None):
+            self.__dict__ = wouldbekwargs
+        else:
+            super().__init__(self)
