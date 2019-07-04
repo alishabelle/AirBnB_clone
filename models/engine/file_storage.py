aabb0065ad6dafc8c83ca5c391c16d407eac8699
alishabelle <alishabelle98@gmail.com>
2019-07-04 03:55:32 +0000

    fixed console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,7 +17,7 @@ class FileStorage:
 
     def save(self):
         new_dict = FileStorage.__objects.copy()
-        with open(FileStorage.__file_path, "w") as write_file:
+        with open(FileStorage.__file_path, "w+") as write_file:
             for key, val in new_dict.items():
                 if not isinstance(val, dict):
                     FileStorage.__objects[key] = val.to_dict()
@@ -27,39 +27,8 @@ class FileStorage:
         import os.path
         from models.base_model import BaseModel
         from models.user import User
-<<<<<<< HEAD
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-        loads = {}
-        new_dict = {}
         if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r") as my_file:
-                loads = json.load(my_file)
-                for key, val in loads.items():
-                    new_dict = val
-                    if new_dict['__class__'] == 'BaseModel':
-                        okay = BaseModel(None, **new_dict)
-                    if new_dict['__class__'] == 'User':
-                        okay = User(None, **new_dict)
-                    if new_dict['__class__'] == 'State':
-                        okay = State(None, **new_dict)  
-                    if new_dict['__class__'] == 'City':
-                        okay = City(None, **new_dict)  
-                    if new_dict['__class__'] == 'Amenity':
-                        okay = Amenity(None, **new_dict)  
-                    if new_dict['__class__'] == 'Place':
-                         okay = Place(None, **new_dict)  
-                    if new_dict['__class__'] == 'Review':
-                         okay = Review(None, **new_dict)  
-                    loads[key] = okay
-                FileStorage.__objects = loads
-=======
-
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r") as my_file:
+            with open(FileStorage.__file_path, "r+") as my_file:
                 FileStorage.__objects = json.load(my_file)
         for key, val in FileStorage.__objects.items():
             if 'BaseModel' in key:
@@ -67,4 +36,3 @@ class FileStorage:
             elif 'User' in key:
                 new_obj = User(None, **val)
             FileStorage.__objects[key] = new_obj
->>>>>>> f75dc79cc34dfc5963e05ef727d433ed8a4ba590
--- models/user.py
+++ models/user.py
@@ -4,17 +4,8 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-<<<<<<< HEAD
     """ user class basemodel is parent """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-=======
-
-    def __init__(self):
-        self.email = ''
-        self.password = ''
-        self.first_name = ''
-        self.last_name = ''
->>>>>>> f75dc79cc34dfc5963e05ef727d433ed8a4ba590
